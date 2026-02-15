df76d1c59c5ed79c695bb5c6dfca43c75be097d6
Moditha Piyumal Wijeratne <100229199+moditha-piyumal@users.noreply.github.com>
2026-02-15 22:39:47 +0530

    Add optional tag flow with IPC-backed storage

--- preload.js
+++ preload.js
@@ -1,7 +1,9 @@
 const { contextBridge, ipcRenderer } = require("electron");
 
 contextBridge.exposeInMainWorld("ideaAPI", {
-	saveIdea: (idea) => ipcRenderer.invoke("save-idea", idea),
+	saveIdea: (ideaText, selectedTag) => ipcRenderer.invoke("save-idea", ideaText, selectedTag),
 	saveDraft: (text) => ipcRenderer.invoke("save-draft", text),
 	loadDraft: () => ipcRenderer.invoke("load-draft"),
+	getTags: () => ipcRenderer.invoke("get-tags"),
+	createTag: (tagName) => ipcRenderer.invoke("create-tag", tagName),
 });
